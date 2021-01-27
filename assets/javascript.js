118e7d8067a25ee3aa9e9f1cba982a0b698de6ed
vanscoyro <35547605+vanscoyro@users.noreply.github.com>
2021-01-26 19:16:54 -0500

    initial commit

---
+++ assets/javascript.js
@@ -0,0 +1,31 @@
+
+
+
+// restful API test code
+// const settings = {
+// 	"async": true,
+// 	"crossDomain": true,
+// 	"url": "https://opentripmap-places-v1.p.rapidapi.com/%7Blang%7D/places/xid/%7Bxid%7D",
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-key": "6401d668c2msh3375a8212e10ddep179156jsn717e7d30372a",
+// 		"x-rapidapi-host": "opentripmap-places-v1.p.rapidapi.com"
+// 	}
+// };
+// $.ajax(settings).done(function (response) {
+// 	console.log(response);
+// });
+
+// $('#searchBtn').on("click", function(){
+//     let query = $("#searchLocation").val();
+//     let queryURL = ""
+    
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET"
+//     }).then(function (response)) {
+//         console.log(response);
+//     }
+    
+// })
+
