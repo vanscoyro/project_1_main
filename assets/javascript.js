8f3543aba23769c3c1e43e3e6a98f70237a0bdf2
Lucas Diaz <lucasdiaz22b@gmail.com>
2021-01-28 20:03:24 -0500

    added github api functionality

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,31 +1,26 @@
+var url = "https://api.github.com/users/";
+var settings = {
+    "method": "GET",
+    "timeout": 0,
+    "headers": {
+      "Authorization": "Bearer cd0be9b8784ec0c3cc9202784dfccd8a72fbf953"
+    },
+  };
+  
+$("#submit").click(function(){
+    alert();
+        // callGithub();
+});
 
 
-
-// restful API test code
-// const settings = {
-// 	"async": true,
-// 	"crossDomain": true,
-// 	"url": "https://opentripmap-places-v1.p.rapidapi.com/%7Blang%7D/places/xid/%7Bxid%7D",
-// 	"method": "GET",
-// 	"headers": {
-// 		"x-rapidapi-key": "6401d668c2msh3375a8212e10ddep179156jsn717e7d30372a",
-// 		"x-rapidapi-host": "opentripmap-places-v1.p.rapidapi.com"
-// 	}
-// };
-// $.ajax(settings).done(function (response) {
-// 	console.log(response);
-// });
-
-// $('#searchBtn').on("click", function(){
-//     let query = $("#searchLocation").val();
-//     let queryURL = ""
-    
-//     $.ajax({
-//         url: queryURL,
-//         method: "GET"
-//     }).then(function (response)) {
-//         console.log(response);
-//     }
-    
-// })
-
+function callGithub(){
+  
+  var username = $("#github").val();
+  settings.url = url+username ; 
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+        var image =  $("<img>")
+        image.attr("src",response.avatar_url);
+        $("body").append(image);
+      });
+}
