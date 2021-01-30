af596da1dc53b801a51d84fefcd8606cafef393c
Rob V <rvans003@fiu.edu>
2021-01-30 13:45:07 -0500

    fully functional API calls with atob functionality

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,43 +1,49 @@
 
 
+//github api call
 var url = "https://api.github.com/users/";
+var githubApiKey = atob("MDk1ZWQyYjMwOThhNmE1N2UxMWI3NTc3ZWE0NjU4YjRhMmE0Nzk0Mg==")
+
 var settings = {
     "method": "GET",
     "timeout": 0,
     "headers": {
-      "Authorization": "Bearer 095ed2b3098a6a57e11b7577ea4658b4a2a47942"
+      "Authorization": "Bearer "+ githubApiKey,
     },
   };
 $("#submit").click(function(){
         callGithub();
 });
 
+
+//callGithub() function
+function callGithub(){
+  
+
+  var username = $("#github").val();
+  settings.url = url+username ; 
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+        $("#github-img").attr("src",response.avatar_url);
+      });
+}
+
+
 //google sheets API request call
 var urlSheet = "https://sheets.googleapis.com/v4/spreadsheets/10KzkFG-9gv5m5-PrkunguT3BvJwU8kA01Vi0WOovg_8/values/FormResponse1!A2:Q1000?key="
-var apiKeySheet = "QUl6YVN5RHJqdEFXdzRRZ2JlNWl0WHpWTUZLTGhmYW1ia3M0NEpv"
+var apiKeySheet = atob("QUl6YVN5RHJqdEFXdzRRZ2JlNWl0WHpWTUZLTGhmYW1ia3M0NEpv")
 
 
 var settings = {
   "method": "GET",
   "timeout": 0,
 };
-settings.url = urlSheet + atob(apiKeySheet)
+settings.url = urlSheet + apiKeySheet
 $.ajax(settings).done(function (response) {
   console.log(response);
 });
 
 
-
-function callGithub(){
-  
-
-   var username = $("#github").val();
-   settings.url = url+username ; 
-     $.ajax(settings).done(function (response) {
-         console.log(response);
-         $("#github-img").attr("src",response.avatar_url);
-       });
- }
 
 
 $('#form').submit(function(e){
